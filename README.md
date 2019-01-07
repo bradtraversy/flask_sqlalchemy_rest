1e83f98504699521e2fdf934bddc07673dc894be
Brad Traversy <traversymedia@gmail.com>
2019-01-07 11:28:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# REST API With Flask & SQL Alchemy
+
+> Products API using Python Flask, SQL Alchemy and Marshmallow
+
+## Quick Start Using Pipenv
+
+``` bash
+# Activate venv
+$ pipenv shell
+
+# Install dependencies
+$ pipenv install
+
+# Create DB
+$ python
+>> from app import db
+>> db.create_all()
+>> exit()
+
+# Run Server (http://localhst:5000)
+python app.py
+```
+
+## Endpoints
+
+* GET /product
+* GET /product/:id
+* POST (Protected) /product
+* PUT (Protected) /product/:id
+* DELETE (Protected) /product/:id
---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+import os
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
+  price = db.Column(db.Float)
+  qty = db.Column(db.Integer)
+
+  def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
+
+# Product Schema
+class ProductSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'description', 'price', 'qty')
+
+# Init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+# Create a Product
+@app.route('/product', methods=['POST'])
+def add_product():
+  name = request.json['name']
+  description = request.json['description']
+  price = request.json['price']
+  qty = request.json['qty']
+
+  new_product = Product(name, description, price, qty)
+
+  db.session.add(new_product)
+  db.session.commit()
+
+  return product_schema.jsonify(new_product)
+
+# Get All Products
+@app.route('/product', methods=['GET'])
+def get_products():
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  return jsonify(result.data)
+
+# Get Single Products
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+  product = Product.query.get(id)
+  return product_schema.jsonify(product)
+
+# Update a Product
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+  product = Product.query.get(id)
+
+  name = request.json['name']
+  description = request.json['description']
+  price = request.json['price']
+  qty = request.json['qty']
+
+  product.name = name
+  product.description = description
+  product.price = price
+  product.qty = qty
+
+  db.session.commit()
+
+  return product_schema.jsonify(product)
+
+# Delete Product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+  product = Product.query.get(id)
+  db.session.delete(product)
+  db.session.commit()
+
+  return product_schema.jsonify(product)
+
+# Run Server
+if __name__ == '__main__':
+  app.run(debug=True)
