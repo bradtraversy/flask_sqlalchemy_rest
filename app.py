15cd314fa2ee61d228868058dcd6fc22d4835ca7
Gaspard Loriot-Boserup <gaspardloriot@gmail.com>
2021-11-20 16:12:03 +0000

    feat:pgsql

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gitpod@localhost/postgres')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
