59b3a896c64afd7f39cf46d35d9c585e1e464d68
Gaspard Loriot-Boserup <gaspardloriot@gmail.com>
2021-12-22 22:30:08 +0000

    feat:env files setup

--- src/config/firebase.py
+++ src/config/firebase.py
@@ -1,26 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
-from dotenv import dotenv_values
 import json
 
-# pull env variables
-config = dotenv_values(".env")
 
 # cred file path
 cred_file_path = './src/config/esg-analytics.json'
 
 cred_object = {
-    "type": config['TYPE'],
-    "project_id": config['PROJECT_ID'],
-    "private_key_id": config['PRIVATE_KEY_ID'],
-    "private_key": config['PRIVATE_KEY'],
-    "client_email": config['CLIENT_EMAIL'],
-    "client_id": config['CLIENT_ID'],
-    "auth_uri": config['AUTH_URI'],
-    "token_uri": config['TOKEN_URI'],
-    "auth_provider_x509_cert_url": config['AUTH_PROVIDER_X509_CERT_URL'],
-    "client_x509_cert_url": config['CLIENT_X509_CERT_URL'],
+    "type": os.environ['TYPE'],
+    "project_id": os.environ['PROJECT_ID'],
+    "private_key_id": os.environ['PRIVATE_KEY_ID'],
+    "private_key": os.environ['PRIVATE_KEY'],
+    "client_email": os.environ['CLIENT_EMAIL'],
+    "client_id": os.environ['CLIENT_ID'],
+    "auth_uri": os.environ['AUTH_URI'],
+    "token_uri": os.environ['TOKEN_URI'],
+    "auth_provider_x509_cert_url": os.environ['AUTH_PROVIDER_X509_CERT_URL'],
+    "client_x509_cert_url": os.environ['CLIENT_X509_CERT_URL'],
 }
 
 
